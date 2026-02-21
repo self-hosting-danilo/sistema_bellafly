784c1add740909ce4e80a835b7d8921661003851
Danilotec <danilo0123499@gmail.com>
2026-02-21 14:02:32 -0300

    manifest e service worker

---
+++ sistema_bellafly/static/service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "bellafly-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "/",
+        "/static/css/style.css"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
