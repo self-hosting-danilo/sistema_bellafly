6fd313aaaab486818750f96bbe81b9af544c7967
danilotec <danilo0123499@gmail.com>
2026-02-28 11:01:31 -0300

    finalizando estrutra de arquitetura

--- sistema_bellafly/myblog/settings.py
+++ sistema_bellafly/myblog/settings.py
@@ -49,10 +49,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'products',
-    'fidelity',
+    'apps.products',
+    'apps.fidelity',
+    'apps.blog',
     'myblog',
-    'blog',
     'core'
 ]
 
